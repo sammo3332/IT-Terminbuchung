8df84f114f77135560181744f6df848c00d5c4a7
Houssam <houssamtaleb009@gmail.com>
2025-04-23 16:34:28 +0200

    Update App.js

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,17 +1,19 @@
 // src/App.js
 
-import React from 'react';
-import CreateAppointment from './components/CreateAppointment'; // Deine Komponente zum Erstellen von Terminen
-import AppointmentList from './components/AppointmentList';   // Deine Komponente zur Anzeige der Termine
+import React from 'react'
+import ChatBot from './components/ChatBot'
+import CreateAppointment from './components/CreateAppointment'
+import AppointmentList from './components/AppointmentList'
 
-function App() {
+function App () {
   return (
-    <div className="App">
+    <div className='App'>
       <h1>IT Terminbuchung</h1>
-      <CreateAppointment />  {/* Hier kannst du das Formular zum Erstellen von Terminen anzeigen */}
-      <AppointmentList />    {/* Hier zeigst du die Liste der Termine */}
+      <CreateAppointment />
+      <AppointmentList />
+      <ChatBot /> {/* Chatbot wird hier eingebunden */}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
